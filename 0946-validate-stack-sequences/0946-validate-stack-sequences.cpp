8179315ef636bf51a2605fd1ec27d1192800f45e
miroslav888888 <115403136+miroslav888888@users.noreply.github.com>
2024-03-19 03:02:12 +0800

    Time: 0 ms (100%), Space: 17.8 MB (90.04%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
-        int n=pushed.size();
+        int n = pushed.size();
         vector<int> temp;
-        int j=0;
-            for(int i=0;i<n;i++){
-                temp.push_back(pushed[i]);
-                while(popped[j]==temp[temp.size()-1]){
-                    temp.pop_back();
-                    j++;
-                    if(temp.size()==0) break;
-                   
-                }
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            temp.push_back(pushed[i]);
+            while (popped[j] == temp[temp.size() - 1]) {
+                temp.pop_back();
+                j++;
+                if (temp.size() == 0)
+                    break;
             }
-        return (temp.size()==0);
+        }
+        return (temp.size() == 0);
     }
 };
